d7c175f68a56df5bfed1b3d6d7717db5ca2fd351
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-08-05 20:29:32 +0200

    Oops. Had missed that my new javascript file was excluded by .gitignore...

---
+++ collection.media/__magnus_js.js
@@ -0,0 +1,44 @@
+﻿loadScripts(['https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js'], function () {
+   document.documentElement.setAttribute('data-glossary-layout-mode', 'compact');
+   document.documentElement.setAttribute('data-theme', 'dark');
+
+
+   function selectText(element) {
+      var range = document.createRange();
+      range.selectNodeContents(element);
+      var selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+   }
+
+   function removeSelection(element) { window.getSelection().removeAllRanges(); }
+
+   var clipboard = new ClipboardJS('.clipboard', {
+      text: function (trigger) {
+         setTimeout(()  => selectText(trigger));
+         return trigger.innerText;
+      }
+   });
+});
+
+
+function loadScripts(urls, callback) {
+   let index = 0;
+
+   function loadNextScript() {
+      if (index < urls.length) {
+         fetch(urls[index])
+            .then(response => response.text())
+            .then(text => {
+               eval(text);
+               index++;
+               loadNextScript();
+            })
+            .catch(error => console.error(error));
+      } else {
+         callback();
+      }
+   }
+
+   loadNextScript();
+}
